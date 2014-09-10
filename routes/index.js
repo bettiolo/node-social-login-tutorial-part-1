373e49eeda6f808dece64161dbecbdc998aaceec
Marco Bettiolo <marco@bettiolo.it>
2014-09-10 18:13:37 +0100

    Using environment vars for oauth keys

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: 'Express',
+      googleClientId: process.env.GOOGLE_CLIENT_ID
+  });
 });
 
 module.exports = router;
