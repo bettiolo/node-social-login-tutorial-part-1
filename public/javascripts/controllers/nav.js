0acd2bd69800642284dac2c5e881a2393ae392b6
Marco Bettiolo <marco@bettiolo.it>
2014-12-16 19:13:09 +0000

    Style improvements

--- public/javascripts/controllers/nav.js
+++ public/javascripts/controllers/nav.js
@@ -3,8 +3,8 @@ app.controller('navController', function ($scope, $location, loginService) {
 	$scope.login = loginService;
 	$scope.getProfileImageUrl = function () {
 		if ($scope.login && $scope.login.user) {
-			return $scope.login.user.image.url;
+			return $scope.login.user.image.url.replace('sz=50', 'sz=48');
 		}
-		return '';
+		return 'images/unknown.png';
 	}
 });
